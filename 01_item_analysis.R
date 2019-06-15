e95428e6446793917a94f6f1b8394bb166899ad6
Chihiro Itamiya <chihiroitamiya@Chihiro-no-MacBook-Pro.local>
2019-06-15 18:55:06 +0900

    change mgirt settings

--- 01_item_analysis.R
+++ 01_item_analysis.R
@@ -150,11 +150,26 @@ pars1 <- coef(fit_after_2009, IRTpars = T) # negative discrimi paramter in some 
 
 # UNIDIM IRT with Multiple Group
 const <- c("free_var", "free_means", colnames(mat_after_2009_rev))
+faclev <- dat_after_2009_rev$SURVEY %>% unique %>% .[c(5:10, 1:4)]
+mgmodel <- mirt.model("F1 = 1-85
+                       PRIOR = (1-85, a1, lnorm,  0.0 , 2)
+                       PRIOR = (1-85, d1, norm,  -1.0 , 3)
+                       PRIOR = (1-85, d2, norm,  -0.5 , 3)
+                       PRIOR = (1-85, d3, norm,   0.5 , 3)
+                       PRIOR = (1-85, d4, norm,   1.0 , 3)
+                      ")
+set.seed(0204)
 fit_mg_after_2009 <- mat_after_2009_rev %>% # .[colSums(., na.rm = T) != 0, ] %>% 
-  multipleGroup(model = mirt.model("F1 = 1-85"), group = dat_after_2009_rev$SURVEY, invariance = const,
-                technical = list(removeEmptyRows = TRUE))
+  multipleGroup(model = mgmodel, 
+                group = dat_after_2009_rev$SURVEY %>% factor(levels = faclev), 
+                invariance = const,
+                optimizer = "BFGS", # "NR",
+                dentype = "empiricalhist",
+                GenRandomPars = TRUE, # 初期値の計算で、識別力に負の値が生成されるらしい
+                accelarete = "squarem",
+                technical = list(removeEmptyRows = TRUE, 
+                                 NCYCLES = 1000))
+plot(fit_mg_after_2009, type = "empiricalhist")
 
-
-
-
+pars2 <- coef(fit_mg_after_2009, IRTpars = F)
 
