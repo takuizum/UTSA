665e9ce007064f2281242e9f2b77f39140a3c052
takuizum <40698749+takuizum@users.noreply.github.com>
2019-06-21 12:03:41 +0900

    add FIFA analysis

---
+++ R/03_FIFA_analysis.R
@@ -0,0 +1,18 @@
+# FIFA
+
+
+load("data/fafit.Rdata")
+# データが大きすぎるので，.Rdataに記録しないように削除
+rm(list = c("fa1fit_after_2009", "fa2fit_after_2009", "fa3fit_after_2009", "fa4fit_after_2009")) 
+
+anova(fa1fit_after_2009, fa2fit_after_2009)
+anova(fa2fit_after_2009, fa3fit_after_2009)
+anova(fa3fit_after_2009, fa4fit_after_2009)
+
+# F = 1
+fa1fit_after_2009 %>% extract.mirt(what = "F")
+fa1fit_after_2009 %>% extract.mirt(what = "h2")
+
+# F = 2
+fa2fit_after_2009 %>% extract.mirt(what = "F")
+fa2fit_after_2009 %>% extract.mirt(what = "h2")
