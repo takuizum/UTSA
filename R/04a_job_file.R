29460e52dfdcd313016227bd7db1edd9b2f88258
takuizum <40698749+takuizum@users.noreply.github.com>
2019-06-25 17:18:48 +0900

    fix

--- R/04a_job_file.R
+++ R/04a_job_file.R
@@ -3,8 +3,9 @@ library(tidyverse);library(rstan)
 load("data/data.Rdata")
 
 # detect the number of using cores
-options(mc.cores = 10)
+options(mc.cores = parallel::detectCores() - 1)
 rstan_options(auto_write = TRUE)
+Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
 
 # data set 
 list_2009_3 <- list(Y = mat_after_2009_rev %>% as_tibble %>% select(-remove_names, -remove_names_fa) %>% map_df(function(x){x[is.na(x)] <- 999;as.integer(x)}) %>% as.matrix, 
@@ -20,15 +21,15 @@ list_2009_3 <- list(Y = mat_after_2009_rev %>% as_tibble %>% select(-remove_name
 grm_mg2 <- stan_model("stan_model/grm_mg_2.stan") # stan_model("~takuizum/local_documents/R/rstan/rstan/grm_mg.stan")# 
 
 cat("VB NOW....\n")
-# fit_after_2009_vb <- vb(data = list_2009_3, object = grm_mg2)
+fit_after_2009_vb <- vb(data = list_2009_3, object = grm_mg2, init = 1)
 
 cat("MCMC NOW....\n")
-# inits <- list("a" = fit_after_2009_vb %>% rstan::extract() %$% a %>% apply(2, mean),
-#               "d" = fit_after_2009_vb %>% rstan::extract() %$% d %>% apply(c(2,3), mean) %>% matrix(ncol = 4, nrow = 53),
-#               "theta" = fit_after_2009_vb %>% rstan::extract() %$% theta %>% apply(2, mean),
-#               "mu_d" = matrix(1, ncol = 5, nrow = 53),
-#               "mu_free" = rep(0, 9),
-#               "sigma_free" =  rep(1, 9))
-fit_after_2009_mcmc <- sampling(data = list_2009_3, object = grm_mg2, seed = 0204)#, init = list(inits, inits, inits, inits))
+inits <- list("a" = fit_after_2009_vb %>% rstan::extract() %$% a %>% apply(2, mean),
+              "d" = fit_after_2009_vb %>% rstan::extract() %$% d %>% apply(c(2,3), mean) %>% matrix(ncol = 4, nrow = 53),
+              "theta" = fit_after_2009_vb %>% rstan::extract() %$% theta %>% apply(2, mean),
+              "mu_d" = matrix(1, ncol = 5, nrow = 53),
+              "mu_free" = rep(0, 9),
+              "sigma_free" =  rep(1, 9))
+fit_after_2009_mcmc <- sampling(data = list_2009_3, object = grm_mg2, seed = 0204, init = list(inits, inits, inits, inits))
 save(c("fit_after_2009_mcmc", "fit_after_2009_vb"), file = "data/mcmc1.Rdata")
 
